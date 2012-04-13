d4209b2cbd459cb1a2b11c224089849ea99c8fd6
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-04-13 13:06:54 +0200

    [REFACTOR] Simplification
    the expression (p-1)/q evaluates to 2. this fact is used to simplify
    the method. Now a pow method is not necessary any more.

--- pocklington/criteria.py
+++ pocklington/criteria.py
@@ -4,7 +4,7 @@ class PrimalityTester:
         p = 2 * q + 1           # p satisfies q | p-1
         a = self.candidateTo(p) # s satisfies a**(p-1) % p == 1 mod p        
         
-        return self.gcd(self.pow(a,(p-1)/q) - 1, p) == 1
+        return self.gcd(a*a - 1, p) == 1
 
     def candidateTo(self, p):
         """Return an a such that a**(p-1) % p == 1"""
@@ -22,15 +22,6 @@ class PrimalityTester:
             exponent, power = exponent / 2, power**2 % modulus
         return result
 
-    def pow(self, a, exponent):
-        """Return a**exponent"""
-        result, power = 1, a
-        while (exponent > 0):
-            if (exponent % 2 != 0):
-                result = (result * power)
-            exponent, power = exponent / 2, power**2
-        return result
-    
     def gcd(self, a, b):
         while (b != 0):
             a, b = b, a % b
