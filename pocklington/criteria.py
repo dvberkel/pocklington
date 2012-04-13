b4d16877c442d3fff488431944ad2569f54cc36a
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-04-13 07:26:11 +0200

    Pocklington Criteria Implemented
    The Pocklington Criteria is implemented in python.

--- pocklington/criteria.py
+++ pocklington/criteria.py
@@ -1,3 +1,23 @@
 class PrimalityTester:
     def isGermainPrime(self, q):
-        return True
+        """Determines if q is a Sophie Germain prime"""
+        p = 2 * q + 1           # p satisfies q | p-1
+        a = self.candidateTo(p) # s satisfies a**(p-1) % p == 1 mod p        
+        
+        return self.gcd(a**((p-1)/q) - 1, p) == 1
+
+    def candidateTo(self, p):
+        """Return an a such that a**(p-1) % p == 1"""
+        a = 2
+        while (self.powmod(a, p-1, p) != 1):
+            a += 1
+        return a
+
+    def powmod(self, a, exponent, modulus):
+        """Return a**exponent % modulus"""
+        return a**exponent % modulus
+    
+    def gcd(self, a, b):
+        while (b != 0):
+            a, b = b, a % b
+        return a
--- pocklington/test/criteria.py
+++ pocklington/test/criteria.py
@@ -3,9 +3,20 @@ import unittest
 from pocklington.criteria import PrimalityTester
 
 class testCriteria(unittest.TestCase):
-    def testGermainPrime(self):
-        tester = PrimalityTester()
-        self.assertTrue(tester.isGermainPrime(5))
+    def setUp(self):
+        self.tester = PrimalityTester();
+
+    def testPositiveGermainPrime(self):
+        self.assertTrue(self.tester.isGermainPrime(2))
+        self.assertTrue(self.tester.isGermainPrime(3))
+        self.assertTrue(self.tester.isGermainPrime(5))
+        self.assertTrue(self.tester.isGermainPrime(11))
+
+    def testNegativeGermainPrime(self):
+        self.assertFalse(self.tester.isGermainPrime(7))
+        self.assertFalse(self.tester.isGermainPrime(13))
+        self.assertFalse(self.tester.isGermainPrime(17))
+        self.assertFalse(self.tester.isGermainPrime(19))
 
 if __name__ == '__main__':
     unittest.main()
